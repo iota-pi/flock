9f7dfe2bd5b32e3f46df9ffe493d79c8893fe209
David Adams <david.adams@cross-code.org>
2021-08-10 21:19:45 +1000

    Run cypress tests before deploying

--- deploy/run-all-tests.sh
+++ deploy/run-all-tests.sh
@@ -2,10 +2,15 @@
 set -euo pipefail
 cd "$(dirname "$(realpath "$0")")/.."
 
-export CI=1
+export CI=true
 (
   cd app
   yarn test
+
+  DISABLE_ESLINT_PLUGIN=true BROWSER=none yarn start >/dev/null 2>&1 &
+  server=$!
+  yarn run cypress run
+  kill $! >/dev/null 2>&1 || true
 )
 (
   cd vault
