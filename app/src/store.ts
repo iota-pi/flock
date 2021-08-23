fdf81ae6f31a01beba57910faf9bcd71a04bd1db
David Adams <david.adams@cross-code.org>
2021-08-23 15:51:38 +1000

    Disable React devtools on production builds

--- app/src/store.ts
+++ app/src/store.ts
@@ -1,12 +1,15 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { RootState, AllActions, rootReducer } from './state';
 
+const aptCompose: typeof composeWithDevTools = (
+  process.env.NODE_ENV === 'production' ? compose : composeWithDevTools
+);
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
+  aptCompose(
     applyMiddleware(thunk as ThunkMiddleware<RootState, AllActions>),
   ),
 );
