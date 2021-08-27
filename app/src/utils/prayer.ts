ceea1dc6cac52438019ae198ce74d56dcc9050be
David Adams <david.adams@cross-code.org>
2021-08-28 07:01:23 +1000

    Keep prayed-for items in todays prayer schedule

--- app/src/utils/prayer.ts
+++ app/src/utils/prayer.ts
@@ -20,14 +20,15 @@ export function getLastPrayedFor(
 
 export function getPrayerSchedule(items: Item[]) {
   const activeItems = filterArchived(items);
-  const withNextSchedule: [Item, number][] = activeItems.map(
+  const withNextSchedule: [Item, number, boolean][] = activeItems.map(
     item => [
       item,
       getLastPrayedFor(item, true) + frequencyToMilliseconds(item.prayerFrequency),
+      isSameDay(new Date(), new Date(item.prayedFor[item.prayedFor.length - 1])),
     ],
   );
   withNextSchedule.sort(
-    (a, b) => (a[1] - b[1]) || compareItems(a[0], b[0]),
+    (a, b) => (+b[2] - +a[2]) || (a[1] - b[1]) || compareItems(a[0], b[0]),
   );
   const itemsBySchedule = withNextSchedule.map(x => x[0]);
   return itemsBySchedule;
