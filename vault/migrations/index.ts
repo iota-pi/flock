fbc3cf0f07ab665fea933e78a8dfafb0ba6b6ae6
David Adams <david.adams@cross-code.org>
2021-08-30 23:07:41 +1000

    More migrations

--- vault/migrations/index.ts
+++ vault/migrations/index.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import { createHash } from 'crypto';
 import { ACCOUNT_TABLE_NAME, getConnectionParams } from '../drivers/dynamo';
 
 const client = new AWS.DynamoDB.DocumentClient(getConnectionParams());
@@ -19,6 +20,62 @@ const migrations: { [name: string]: () => Promise<void> } = {
           ExpressionAttributeValues: {
             ':token': authToken,
           },
+        }).promise();
+      }
+      console.log(`Updated ${items.length} items`);
+    }
+  },
+  async hashAuthToken () {
+    const results = await client.scan({
+      TableName: ACCOUNT_TABLE_NAME,
+      AttributesToGet: ['account', 'authToken'],
+    }).promise();
+    const items = results.Items as { account: string, authToken: string }[] | undefined;
+    if (items) {
+      for (const { account, authToken } of items) {
+        const hash = createHash('sha512');
+        hash.update(Buffer.from(authToken, 'utf8'));
+        await client.update({
+          TableName: ACCOUNT_TABLE_NAME,
+          Key: { account },
+          UpdateExpression: 'SET hashAuthToken = :token',
+          ExpressionAttributeValues: {
+            ':token': hash.digest().toString('base64'),
+          },
+        }).promise();
+      }
+      console.log(`Updated ${items.length} items`);
+    }
+  },
+  async installHashAuthToken () {
+    const results = await client.scan({
+      TableName: ACCOUNT_TABLE_NAME,
+      AttributesToGet: ['account'],
+    }).promise();
+    const items = results.Items as { account: string, authToken: string }[] | undefined;
+    if (items) {
+      for (const { account } of items) {
+        await client.update({
+          TableName: ACCOUNT_TABLE_NAME,
+          Key: { account },
+          UpdateExpression: 'SET authToken = hashAuthToken',
+        }).promise();
+      }
+      console.log(`Updated ${items.length} items`);
+    }
+  },
+  async cleanUpAuthToken () {
+    const results = await client.scan({
+      TableName: ACCOUNT_TABLE_NAME,
+      AttributesToGet: ['account'],
+    }).promise();
+    const items = results.Items as { account: string }[] | undefined;
+    if (items) {
+      for (const { account } of items) {
+        await client.update({
+          TableName: ACCOUNT_TABLE_NAME,
+          Key: { account },
+          UpdateExpression: 'REMOVE rawAuthToken, hashAuthToken',
         }).promise();
       }
       console.log(`Updated ${items.length} items`);
