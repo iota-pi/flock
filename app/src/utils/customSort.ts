d7d8ecff58c2f12064cf90361b730bcefc48fdff
David Adams <david.adams@cross-code.org>
2021-10-17 10:23:17 +1100

    Automatically sort by archived and item type

--- app/src/utils/customSort.ts
+++ app/src/utils/customSort.ts
@@ -1,11 +1,12 @@
-import { compareIds, getItemName, Item } from '../state/items';
+import { compareIds, getItemName, Item, ITEM_TYPES } from '../state/items';
 
 export type CriterionName = (
   'archived' |
   'created' |
   'description' |
   'lastName' |
-  'name'
+  'name' |
+  'type'
 );
 export interface SortCriterion {
   type: CriterionName,
@@ -15,21 +16,28 @@ export interface CriterionDisplay {
   name: string,
   normal: string,
   reverse: string,
+  hide?: boolean,
 }
 export const CRITERIA_DISPLAY_MAP: Record<CriterionName, CriterionDisplay> = {
-  archived: { name: 'Archived', normal: 'Archived last', reverse: 'Archived first' },
+  archived: { name: 'Archived', normal: 'Archived last', reverse: 'Archived first', hide: true },
   created: { name: 'Date created', normal: 'Recent first', reverse: 'Recent last' },
   description: { name: 'Description', normal: 'Ascending', reverse: 'Descending' },
   lastName: { name: 'Last Name', normal: 'Ascending', reverse: 'Descending' },
   name: { name: 'Name', normal: 'Ascending', reverse: 'Descending' },
+  type: { name: 'Item type', normal: 'Ascending', reverse: 'Descending', hide: true },
 };
-export const CRITERIA_DISPLAY = Object.entries(CRITERIA_DISPLAY_MAP).sort(
+export const CRITERIA_DISPLAY = Object.entries(CRITERIA_DISPLAY_MAP).filter(
+  ([, { hide }]) => !hide,
+).sort(
   ([a], [b]) => a.localeCompare(b),
 ) as [CriterionName, CriterionDisplay][];
 
 export const DEFAULT_CRITERIA: SortCriterion[] = [
-  { type: 'archived', reverse: false },
   { type: 'name', reverse: false },
+];
+export const AUTOMATIC_CRITERIA: SortCriterion[] = [
+  { type: 'archived', reverse: false },
+  { type: 'type', reverse: false },
 ];
 
 const CRITERION_FUNCTIONS: Record<CriterionName, (a: Item, b: Item) => number> = {
@@ -43,10 +51,12 @@ const CRITERION_FUNCTIONS: Record<CriterionName, (a: Item, b: Item) => number> =
     return 0;
   },
   name: (a, b) => getItemName(a).localeCompare(getItemName(b)),
+  type: (a, b) => ITEM_TYPES.indexOf(a.type) - ITEM_TYPES.indexOf(b.type),
 };
 
 const compareItems = (criteria: SortCriterion[]) => (a: Item, b: Item) => {
-  for (const criterion of criteria) {
+  const allCriteria = [...AUTOMATIC_CRITERIA, ...criteria];
+  for (const criterion of allCriteria) {
     const func = CRITERION_FUNCTIONS[criterion.type];
     const result = func(a, b);
     if (result) {
