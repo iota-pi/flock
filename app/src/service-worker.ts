f5cf0c142f55aa1e6032b9041fc801cee0325215
David Adams <david.adams@cross-code.org>
2022-01-02 09:47:55 +1100

    We don't need to double-send messages!

--- app/src/service-worker.ts
+++ app/src/service-worker.ts
@@ -2,27 +2,12 @@
 /* eslint-disable no-restricted-globals */
 
 import { initializeApp } from 'firebase/app';
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
 import firebaseConfig from './utils/firebase-config';
 
 declare const self: ServiceWorkerGlobalScope;
 
 function initServiceWorker() {
-  const app = initializeApp(firebaseConfig);
-  const messaging = getMessaging(app);
-  onBackgroundMessage(messaging, async () => {
-    // Customize notification here
-    const notificationTitle = 'Prayer reminder';
-    const notificationOptions = {
-      body: 'Pray for the flock!',
-      icon: '/flock.png',
-    };
-
-    self.registration.showNotification(
-      notificationTitle,
-      notificationOptions,
-    );
-  });
+  initializeApp(firebaseConfig);
 }
 
 initServiceWorker();
