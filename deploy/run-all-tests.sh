592beb4cde0a05c5e9c13d15fc8fd61ef60677a1
David Adams <david.adams@cross-code.org>
2024-05-13 07:11:18 +1000

    Don't run e2e tests

--- deploy/run-all-tests.sh
+++ deploy/run-all-tests.sh
@@ -8,15 +8,15 @@ export CI=true
   cd app
   yarn test
 
-  if grep -qe 'it\.only(' cypress/e2e/*.cy.ts; then
-    >&2 echo 'Cannot deploy; it.only() was found in Cypress tests'
-    exit 1
-  fi
+  # if grep -qe 'it\.only(' cypress/e2e/*.cy.ts; then
+  #   >&2 echo 'Cannot deploy; it.only() was found in Cypress tests'
+  #   exit 1
+  # fi
 
-  yarn build
-  yarn serve &
-  yarn cypress run
-  kill $! >/dev/null 2>&1 || true
+  # yarn build
+  # yarn serve &
+  # yarn cypress run
+  # kill $! >/dev/null 2>&1 || true
 )
 (
   cd vault
