53bdf49bfd4c51a47eb98fe990efb23191c076c0
David Adams <david.adams@cross-code.org>
2025-11-26 16:56:27 +1100

    Merge setupTests files

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -44,3 +44,9 @@ if (typeof (globalThis as any).TextDecoder === 'undefined') {
     configurable: true,
   })
 }
+
+// Ensure `DYNAMODB_ENDPOINT` is set when running tests locally from the host
+// Default to the common DynamoDB Local address used by `docker compose`
+if (!process.env.DYNAMODB_ENDPOINT) {
+  process.env.DYNAMODB_ENDPOINT = 'http://localhost:8000'
+}
--- src/vault/setupTests.ts
+++
@@ -1,5 +0,0 @@
-// Ensure `DYNAMODB_ENDPOINT` is set when running tests locally from the host
-// Default to the common DynamoDB Local address used by `docker compose`
-if (!process.env.DYNAMODB_ENDPOINT) {
-  process.env.DYNAMODB_ENDPOINT = 'http://localhost:8000'
-}
