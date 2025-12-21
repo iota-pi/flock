92be816a553e81d9cfde901a749be397dd4450c2
David Adams <david.adams@cross-code.org>
2025-12-22 08:23:08 +1100

    Remove unused code

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,7 +4,6 @@ import type { OverridableComponent } from '@mui/material/OverridableComponent'
 import AddIcon from '@mui/icons-material/Add'
 import ArchiveIcon from '@mui/icons-material/Archive'
 import BackIcon from '@mui/icons-material/ChevronLeft'
-import CalendarIcon from '@mui/icons-material/Today'
 import DeleteIcon from '@mui/icons-material/DeleteOutline'
 import DownloadIcon from '@mui/icons-material/Download'
 import EditIcon from '@mui/icons-material/Edit'
@@ -13,23 +12,19 @@ import FrequencyIcon from '@mui/icons-material/Schedule'
 import GroupIcon from '@mui/icons-material/Groups'
 import HomeIcon from '@mui/icons-material/Home'
 import MenuIcon from '@mui/icons-material/Menu'
-import MinusIcon from '@mui/icons-material/Remove'
 import NextIcon from '@mui/icons-material/ChevronRight'
 import NotesIcon from '@mui/icons-material/Notes'
 import NotificationIcon from '@mui/icons-material/Notifications'
 import OptionsIcon from '@mui/icons-material/Settings'
 import PasswordIcon from '@mui/icons-material/Password'
 import PersonIcon from '@mui/icons-material/Person'
-import PrayerPointIcon from '@mui/icons-material/FormatListBulleted'
 import RemoveIcon from '@mui/icons-material/Close'
 import ResetIcon from '@mui/icons-material/Replay'
 import SaveIcon from '@mui/icons-material/Check'
 import SearchIcon from '@mui/icons-material/Search'
-import ServerIcon from '@mui/icons-material/Dns'
 import SignOutIcon from '@mui/icons-material/ExitToApp'
 import SortIcon from '@mui/icons-material/Sort'
 import SuccessIcon from '@mui/icons-material/CheckCircle'
-import SuggestIcon from '@mui/icons-material/Update'
 import UnarchiveIcon from '@mui/icons-material/Unarchive'
 import UploadIcon from '@mui/icons-material/Upload'
 import WarningIcon from '@mui/icons-material/Warning'
@@ -47,7 +42,6 @@ export {
   ArchiveIcon,
   BackIcon,
   BackIcon as ContractMenuIcon,
-  CalendarIcon,
   DeleteIcon,
   DownloadIcon,
   EditIcon,
@@ -56,7 +50,6 @@ export {
   GroupIcon,
   HomeIcon,
   MenuIcon,
-  MinusIcon,
   NextIcon,
   NextIcon as ExpandMenuIcon,
   NotesIcon,
@@ -65,16 +58,13 @@ export {
   PasswordIcon,
   PersonIcon,
   PrayerIcon,
-  PrayerPointIcon,
   RemoveIcon,
   ResetIcon,
   SaveIcon,
   SearchIcon,
-  ServerIcon,
   SignOutIcon,
   SortIcon,
   SuccessIcon,
-  SuggestIcon,
   UnarchiveIcon,
   UploadIcon,
   WarningIcon,
