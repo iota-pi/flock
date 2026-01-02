e1fbc75d8830a883663b1c9a10a3c7c822f40a71
David Adams <david.adams@cross-code.org>
2026-01-02 19:31:25 +1100

    Migrate from zxcvbn to zxcvbn-ts

--- src/hooks/usePasswordStrength.ts
+++ src/hooks/usePasswordStrength.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import type { ZXCVBNScore, ZXCVBNResult } from 'zxcvbn'
+import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core'
+import type { ZxcvbnResult } from '@zxcvbn-ts/core'
 import customDomainWords from '../utils/customDomainWords'
 
 const MIN_PASSWORD_LENGTH = 10
@@ -8,21 +9,34 @@ const MIN_PASSWORD_STRENGTH = 3
 export interface PasswordFeedback {
   score: number,
   error: string,
-  feedback: ZXCVBNResult['feedback'],
+  feedback: ZxcvbnResult['feedback'],
 }
 
+let optionsSet = false
 async function scorePassword(password: string) {
-  const { default: zxcvbn } = await import('zxcvbn')
+  if (!optionsSet) {
+    const { dictionary: commonDictionary, adjacencyGraphs } = await import('@zxcvbn-ts/language-common')
+    const { dictionary: enDictionary, translations } = await import('@zxcvbn-ts/language-en')
+    zxcvbnOptions.setOptions({
+      dictionary: {
+        ...commonDictionary,
+        ...enDictionary,
+      },
+      graphs: adjacencyGraphs,
+      translations,
+    })
+    optionsSet = true
+  }
   const mainScore = zxcvbn(password, customDomainWords)
   const harshScore = zxcvbn(password.slice(3), customDomainWords)
-  mainScore.score = Math.min(mainScore.score, harshScore.score) as ZXCVBNScore
+  mainScore.score = Math.min(mainScore.score, harshScore.score) as ZxcvbnResult['score']
   return mainScore
 }
 
 export function usePasswordStrength(password: string): PasswordFeedback {
   const [score, setScore] = useState(0)
   const [error, setError] = useState('')
-  const [feedback, setFeedback] = useState<ZXCVBNResult['feedback']>({ warning: '', suggestions: [] })
+  const [feedback, setFeedback] = useState<ZxcvbnResult['feedback']>({ warning: '', suggestions: [] })
 
   // Reset state if password is empty (i.e. don't score blank passwords)
   if (!password) {
