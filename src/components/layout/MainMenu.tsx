1e035d5ec79ca224c52f385822493f5a13137465
David Adams <david.adams@cross-code.org>
2026-05-26 08:30:47 +1000

    Split MainMenuItem to separate file

--- src/components/layout/MainMenu.tsx
+++ src/components/layout/MainMenu.tsx
@@ -1,29 +1,26 @@
 import { memo, useCallback, useEffect, useMemo, useRef } from 'react'
 import { useNavigate } from 'react-router'
 import {
-  Badge,
   Box,
-  Divider,
   Drawer,
   List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
   styled,
   Toolbar,
 } from '@mui/material'
 import { pages, usePage } from '../pages'
 import { PageId } from '../pages/routes'
-import { ContractMenuIcon, ExpandMenuIcon, MuiIconType } from '../Icons'
+import { ContractMenuIcon, ExpandMenuIcon } from '../Icons'
 import { useNavigationStore } from '../../state/navigationStore'
+import { MainMenuItem } from './MainMenuItem'
 
 export const DRAWER_SPACING_FULL = 30
 export const DRAWER_SPACING_NARROW = 10
 
-interface MinimisedProp {
+export type MinimisedProp = {
   minimised: boolean,
   hasWarning?: boolean,
 }
+
 const StyledDrawer = styled(
   Drawer,
   {
@@ -55,57 +52,6 @@ const FlexList = styled(List)({
   flexDirection: 'column',
   flexGrow: 1,
 })
-const StyledListItemButton = styled(
-  ListItemButton,
-  {
-    shouldForwardProp: p => p !== 'minimised' && p !== 'hasWarning',
-  },
-)<MinimisedProp>(({ minimised, hasWarning, theme }) => ({
-  flexGrow: 0,
-  height: theme.spacing(minimised ? 8 : 6),
-  justifyContent: 'center',
-  transition: theme.transitions.create(['color', 'height']),
-  ...(hasWarning
-    ? {
-      backgroundColor: theme.palette.warning.light,
-      color: theme.palette.warning.contrastText,
-      '&:hover': {
-        backgroundColor: theme.palette.warning.main,
-      },
-      '&.Mui-selected': {
-        backgroundColor: theme.palette.warning.main,
-      },
-    }
-    : {}),
-}))
-const MenuItemIcon = styled(
-  ListItemIcon,
-  {
-    shouldForwardProp: p => p !== 'minimised',
-  },
-)<MinimisedProp>(({ minimised, theme }) => ({
-  color: 'inherit',
-  minWidth: 0,
-  paddingLeft: theme.spacing(minimised ? 1.5 : 0.5),
-  paddingRight: theme.spacing(minimised ? 0 : 3),
-  transition: theme.transitions.create('padding'),
-}))
-const MenuItemText = styled(
-  ListItemText,
-  {
-    shouldForwardProp: p => p !== 'minimised',
-  },
-)<MinimisedProp>(({ minimised, theme }) => ({
-  whiteSpace: 'nowrap',
-  overflowX: 'hidden',
-  textOverflow: 'ellipsis',
-  transition: theme.transitions.create('opacity'),
-  opacity: minimised ? 0 : undefined,
-
-  '& .MuiListItemText-secondary': {
-    display: 'inline',
-  },
-}))
 
 interface Props {
   minimised?: boolean,
@@ -113,67 +59,6 @@ interface Props {
   onMinimise: () => void,
   open: boolean,
 }
-
-type MenuActionId = 'minimise'
-
-interface MainMenuItemProps {
-  dividerBefore?: boolean,
-  icon: MuiIconType,
-  id: PageId | MenuActionId,
-  minimisedMenu: boolean,
-  name: string,
-  onClick: (pageId?: PageId) => void,
-  selected: boolean,
-  warningCount?: number,
-}
-
-
-const MainMenuItem = memo(function MainMenuItem({
-  dividerBefore,
-  icon: Icon,
-  id,
-  minimisedMenu,
-  name,
-  onClick,
-  selected,
-  warningCount = 0,
-}: MainMenuItemProps) {
-  const handleClick = useCallback(
-    () => (id !== 'minimise' ? onClick(id) : onClick()),
-    [id, onClick],
-  )
-
-  return (
-    <>
-      {dividerBefore && (
-        <Divider />
-      )}
-
-      <StyledListItemButton
-        data-cy={`page-${id}`}
-        hasWarning={warningCount > 0}
-        minimised={minimisedMenu}
-        onClick={handleClick}
-        selected={selected}
-      >
-        <MenuItemIcon minimised={minimisedMenu}>
-          {warningCount > 0
-            ? (
-              <Badge badgeContent={warningCount} color="error" max={99}>
-                <Icon />
-              </Badge>
-            )
-            : <Icon />}
-        </MenuItemIcon>
-
-        <MenuItemText
-          minimised={minimisedMenu}
-          primary={name}
-        />
-      </StyledListItemButton>
-    </>
-  )
-})
 
 
 function MainMenu({
---
+++ src/components/layout/MainMenuItem.tsx
@@ -0,0 +1,125 @@
+import { memo, useCallback } from 'react'
+import {
+  Badge,
+  Divider,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+} from '@mui/material'
+import type { MuiIconType } from '../Icons'
+import type { PageId } from '../pages/types'
+import type { MinimisedProp } from './MainMenu'
+
+interface MainMenuItemProps {
+  dividerBefore?: boolean,
+  icon: MuiIconType,
+  id: PageId | MenuActionId,
+  minimisedMenu: boolean,
+  name: string,
+  onClick: (pageId?: PageId) => void,
+  selected: boolean,
+  warningCount?: number,
+}
+
+type MenuActionId = 'minimise'
+
+const StyledListItemButton = styled(
+  ListItemButton,
+  {
+    shouldForwardProp: p => p !== 'minimised' && p !== 'hasWarning',
+  },
+)<MinimisedProp>(({ minimised, hasWarning, theme }) => ({
+  flexGrow: 0,
+  height: theme.spacing(minimised ? 8 : 6),
+  justifyContent: 'center',
+  transition: theme.transitions.create(['color', 'height']),
+  ...(hasWarning
+    ? {
+      backgroundColor: theme.palette.warning.light,
+      color: theme.palette.warning.contrastText,
+      '&:hover': {
+        backgroundColor: theme.palette.warning.main,
+      },
+      '&.Mui-selected': {
+        backgroundColor: theme.palette.warning.main,
+      },
+    }
+    : {}),
+}))
+const MenuItemIcon = styled(
+  ListItemIcon,
+  {
+    shouldForwardProp: p => p !== 'minimised',
+  },
+)<MinimisedProp>(({ minimised, theme }) => ({
+  color: 'inherit',
+  minWidth: 0,
+  paddingLeft: theme.spacing(minimised ? 1.5 : 0.5),
+  paddingRight: theme.spacing(minimised ? 0 : 3),
+  transition: theme.transitions.create('padding'),
+}))
+const MenuItemText = styled(
+  ListItemText,
+  {
+    shouldForwardProp: p => p !== 'minimised',
+  },
+)<MinimisedProp>(({ minimised, theme }) => ({
+  whiteSpace: 'nowrap',
+  overflowX: 'hidden',
+  textOverflow: 'ellipsis',
+  transition: theme.transitions.create('opacity'),
+  opacity: minimised ? 0 : undefined,
+
+  '& .MuiListItemText-secondary': {
+    display: 'inline',
+  },
+}))
+
+
+export const MainMenuItem = memo(function MainMenuItem({
+  dividerBefore,
+  icon: Icon,
+  id,
+  minimisedMenu,
+  name,
+  onClick,
+  selected,
+  warningCount = 0,
+}: MainMenuItemProps) {
+  const handleClick = useCallback(
+    () => (id !== 'minimise' ? onClick(id) : onClick()),
+    [id, onClick],
+  )
+
+  return (
+    <>
+      {dividerBefore && (
+        <Divider />
+      )}
+
+      <StyledListItemButton
+        data-cy={`page-${id}`}
+        hasWarning={warningCount > 0}
+        minimised={minimisedMenu}
+        onClick={handleClick}
+        selected={selected}
+      >
+        <MenuItemIcon minimised={minimisedMenu}>
+          {warningCount > 0
+            ? (
+              <Badge badgeContent={warningCount} color="error" max={99}>
+                <Icon />
+              </Badge>
+            )
+            : <Icon />}
+        </MenuItemIcon>
+
+        <MenuItemText
+          minimised={minimisedMenu}
+          primary={name}
+        />
+      </StyledListItemButton>
+    </>
+  )
+})
